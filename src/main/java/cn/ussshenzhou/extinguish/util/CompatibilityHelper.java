b940bccc4f5ba6877448098b1f31aea4eaab071f
USS-Shenzhou <969840795@qq.com>
2022-07-11 19:22:32 +0800

    optifine compatibility issue - solved - push for backup

---
+++ src/main/java/cn/ussshenzhou/extinguish/util/CompatibilityHelper.java
@@ -0,0 +1,48 @@
+package cn.ussshenzhou.extinguish.util;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.model.data.IModelData;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.*;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import net.minecraftforge.forgespi.language.IModInfo;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Random;
+
+/**
+ * @author USS_Shenzhou
+ */
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class CompatibilityHelper {
+    /**
+     * NoSuchMethodError will be throw if Optifine is installed.
+     * fixed: use tesselateWithAO() with out IModelData
+     * @see net.minecraft.client.renderer.block.ModelBlockRenderer#tesselateWithAO(BlockAndTintGetter, BakedModel, BlockState, BlockPos, PoseStack, VertexConsumer, boolean, Random, long, int)
+     *
+     * @see net.minecraft.client.renderer.block.ModelBlockRenderer#tesselateWithAO(BlockAndTintGetter, BakedModel, BlockState, BlockPos, PoseStack, VertexConsumer, boolean, Random, long, int, IModelData)
+     */
+    @SubscribeEvent
+    public static void optifine(FMLClientSetupEvent event) {
+        /*try {
+            Class c = Class.forName("optifine.OptiFineClassTransformer");
+            IModInfo iModInfo = ModList.get().getModContainerById("extinguish")
+                    .map(ModContainer::getModInfo)
+                    .orElseThrow(() -> new IllegalStateException("ModInfo absent while mod itself is present"));
+            ModLoader.get().addWarning(new ModLoadingWarning(
+                    iModInfo,
+                    ModLoadingStage.SIDED_SETUP,
+                    "warn.extinguish.optifine"
+            ));
+        } catch (ClassNotFoundException ignored) {
+        }*/
+    }
+}
