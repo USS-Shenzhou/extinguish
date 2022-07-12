2deec45c0b31450cf4cb39cb61fc47429265a8d7
USS-Shenzhou <969840795@qq.com>
2022-07-12 12:32:04 +0800

    add warning of ParticleStatus

--- src/main/java/cn/ussshenzhou/extinguish/util/CompatibilityHelper.java
+++ src/main/java/cn/ussshenzhou/extinguish/util/CompatibilityHelper.java
@@ -2,6 +2,8 @@ package cn.ussshenzhou.extinguish.util;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.ParticleStatus;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndTintGetter;
@@ -25,9 +27,10 @@ import java.util.Random;
 public class CompatibilityHelper {
     /**
      * NoSuchMethodError will be throw if Optifine is installed.
+     * <br/>
      * fixed: use tesselateWithAO() with out IModelData
-     * @see net.minecraft.client.renderer.block.ModelBlockRenderer#tesselateWithAO(BlockAndTintGetter, BakedModel, BlockState, BlockPos, PoseStack, VertexConsumer, boolean, Random, long, int)
      *
+     * @see net.minecraft.client.renderer.block.ModelBlockRenderer#tesselateWithAO(BlockAndTintGetter, BakedModel, BlockState, BlockPos, PoseStack, VertexConsumer, boolean, Random, long, int)
      * @see net.minecraft.client.renderer.block.ModelBlockRenderer#tesselateWithAO(BlockAndTintGetter, BakedModel, BlockState, BlockPos, PoseStack, VertexConsumer, boolean, Random, long, int, IModelData)
      */
     @SubscribeEvent
@@ -44,5 +47,20 @@ public class CompatibilityHelper {
             ));
         } catch (ClassNotFoundException ignored) {
         }*/
+    }
+
+    @SubscribeEvent
+    public static void particle(FMLClientSetupEvent event) {
+        if (Minecraft.getInstance().options.particles == ParticleStatus.MINIMAL) {
+            IModInfo iModInfo = ModList.get().getModContainerById("extinguish")
+                    .map(ModContainer::getModInfo)
+                    .orElseThrow(() -> new IllegalStateException("ModInfo absent while mod itself is present"));
+
+            ModLoader.get().addWarning(new ModLoadingWarning(
+                    iModInfo,
+                    ModLoadingStage.SIDED_SETUP,
+                    "warn.extinguish.particle"
+            ));
+        }
     }
 }
