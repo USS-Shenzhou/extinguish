9db64d8c692e465b55ef508ec4f5ac281a74f912
USS-Shenzhou <969840795@qq.com>
2022-07-26 22:52:10 +0800

    performance fix

--- src/main/java/cn/ussshenzhou/extinguish/fire/FireEventListener.java
+++ src/main/java/cn/ussshenzhou/extinguish/fire/FireEventListener.java
@@ -4,6 +4,7 @@ import cn.ussshenzhou.extinguish.blockentities.AutoWaterCannonEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.TimeUtil;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.TickEvent;
@@ -14,11 +15,9 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.Calendar;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -49,17 +48,18 @@ public class FireEventListener {
         LinkedHashSet<BlockPos> fires = FireManager.getFireBuffer().get(level);
         if (fires != null) {
             if (fires.size() > 200) {
-                LogManager.getLogger().warn("Fire buffer is too big with a amount of "
-                        + fires.size()
-                        + " in world "
+                LogManager.getLogger().warn("Fire buffer is too big in world "
                         + level.toString()
                         + " | "
                         + level.dimension());
+                blockPosWarn(fires);
             }
             long mill = System.currentTimeMillis();
             Iterator<BlockPos> iterator = fires.iterator();
             while (iterator.hasNext()) {
                 if (System.currentTimeMillis() > mill + 5) {
+                    LogManager.getLogger().warn("Checking fire buffer is too slow, costing more than 5 ms, skipping remaining fires...");
+                    blockPosWarn(fires);
                     break;
                 }
                 BlockPos firePos = iterator.next();
@@ -75,6 +75,16 @@ public class FireEventListener {
                 }
             }
         }
+    }
+
+    private static void blockPosWarn(LinkedHashSet<BlockPos> fires) {
+        Logger logger = LogManager.getLogger();
+        try{
+            BlockPos randomPos = fires.toArray(new BlockPos[0])[(int) (fires.size() * Math.random())];
+            logger.warn("Here is more information about the warn above:");
+            logger.warn("Fire buffer's length is "+fires.size()+" .");
+            logger.warn("Here is a block randomly chosen from fire buffer.You can check this block at "+randomPos.toShortString()+" in chunk "+(new ChunkPos(randomPos)).toString());
+        } catch (IndexOutOfBoundsException ignored){}
     }
 
     @SubscribeEvent
